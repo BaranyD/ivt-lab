313dd44e91683a54013a71aee13c32bc208ddc34
BaranyD <baranydavid97@gmail.com>
2019-05-16 23:43:03 -0700

    Lab6/1.Feladat

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,31 +10,47 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  private TorpedoStore mockStore1;
+  private TorpedoStore mockStore2;
+
+
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockStore1 = mock(TorpedoStore.class);
+    mockStore2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockStore1, mockStore2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockStore1.fire(1)).thenReturn(true);
+    when(mockStore1.fire(2)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(mockStore1,times(1)).fire(1);
+    verify(mockStore2,times(0)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockStore1.fire(1)).thenReturn(true);
+    when(mockStore2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(mockStore1,times(1)).fire(1);
+    verify(mockStore2,times(1)).fire(1);
   }
 
 }
